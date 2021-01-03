d24974f19ffaa0101d25081e6606f39685965724
asmaahassnasr <asmaa14has@gmail.com>
2021-01-03 17:59:28 +0200

    Adding .env file and test api url

--- src/User/Signup.js
+++ src/User/Signup.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from '../Core/Layout'
+import {API} from '../config'
 
 const Signup = () => {
     return (
             <Layout title=' SignUp Page' description="SignUp for our App">
-               ......
+               {API}
             </Layout>
     )
 }

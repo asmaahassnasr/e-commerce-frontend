3dc9b07e651f83bfd7d1c69e6b2b71582c1c748c
asmaahassnasr <asmaa14has@gmail.com>
2021-01-03 17:10:35 +0200

    Navigation Bar

---
+++ src/Core/Menue.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link, withRouter} from 'react-router-dom'
+
+const Menue = () => {
+    return (
+        <div>
+            <ul className="nav nav-tabs bg-primary">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/signin">Sign In</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/signup">Sign Up</Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default withRouter(Menue);
--- src/Routes.js
+++ src/Routes.js
@@ -3,10 +3,11 @@ import {BrowserRouter,Switch,Route} from 'react-router-dom'
 import Signup from './User/Signup'
 import Signin from './User/Signin'
 import Home from './Core/Home'
-
+import Menue from './Core/Menue'
 const Routes = () =>{
     return (
         <BrowserRouter>
+        <Menue />
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/signin" component={Signin}/>

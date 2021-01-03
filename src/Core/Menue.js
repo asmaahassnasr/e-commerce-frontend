bd555c0e8b553cc6e4b4d0396eac1aa0585f3514
asmaahassnasr <asmaa14has@gmail.com>
2021-01-03 17:16:11 +0200

    Style for Navigation Bar

--- src/Core/Menue.js
+++ src/Core/Menue.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import {Link, withRouter} from 'react-router-dom'
 
-const Menue = () => {
+const isActive = (history,path) => {
+    if(history.location.pathname === path) {
+        return {color: '#ff9900'}
+    }
+    else {
+        return {color:'#fff'}
+    }
+}
+
+const Menue = ({history}) => {
     return (
         <div>
             <ul className="nav nav-tabs bg-primary">
                 <li className="nav-item">
-                    <Link className="nav-link" to="/">Home</Link>
+                    <Link className="nav-link" style={isActive(history,'/')} to="/">Home</Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/signin">Sign In</Link>
+                    <Link className="nav-link" style={isActive(history,'/signin')} to="/signin">Sign In</Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/signup">Sign Up</Link>
+                    <Link className="nav-link" style={isActive(history,'/signup')} to="/signup">Sign Up</Link>
                 </li>
             </ul>
         </div>

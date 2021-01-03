358f27210d455f4e7caab89c79b3fe3c88701da1
asmaahassnasr <asmaa14has@gmail.com>
2021-01-03 21:27:37 +0200

    SignUp Error and success handeling

--- src/User/Signup.js
+++ src/User/Signup.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import Layout from '../Core/Layout'
 import {API} from '../config'
-
+import {Link} from 'react-router-dom'
 
 // put values of submit form in the state , when submit go to backend
 
@@ -15,21 +15,41 @@ const Signup = () => {
         success:false
     });
 
-    const {name,email,password} = values;
+    const {name,email,password,error,success} = values;
 
     const handleChanges = (name) => (event) => {
         setValues({...values,error:false,[name]:event.target.value});
     }
 
     const clickSubmit= (event) =>{
+
         event.preventDefault()
+
+        setValues({...values,error:false})
+
         signup({name,email,password})
+        .then(data => {
+
+            if(data.error){
+                setValues({...values, error:data.error,success:false})
+            }
+            else{
+                setValues({
+                    ...values,
+                    name:"",
+                    email:"",
+                    password:"",
+                    error:"",
+                    success:true
+                })
+            }
+        })
     } 
 
     const signup =(user) =>{
-        // console.log(user);
+
         // Send Data to backend as json using fetch
-        fetch(`${API}/signup`,{ 
+        return fetch(`${API}/signup`,{ 
             method:"POST",
             headers:{
                 Accept:'application/json',
@@ -49,18 +69,46 @@ const Signup = () => {
         <form>
             <div className="form-group">
                 <label className="text-muted">Name</label>
-                <input type="text" onChange={handleChanges('name')} className="form-control" />
+                <input 
+                type="text" 
+                onChange={handleChanges('name')} 
+                className="form-control" 
+                value={name}
+                />
             </div>
             <div className="form-group">
                 <label className="text-muted">Email</label>
-                <input type="email" className="form-control"  onChange={handleChanges('email')} />
+                <input 
+                type="email" 
+                className="form-control"  
+                onChange={handleChanges('email')} 
+                value={email}
+                />
             </div>
             <div className="form-group">
                 <label className="text-muted">Password</label>
-                <input type="password" className="form-control"  onChange={handleChanges('password')} />
+                <input 
+                type="password" 
+                className="form-control"  
+                onChange={handleChanges('password')} 
+                value={password}
+                />
             </div>
             <button onClick={clickSubmit} className="btn btn-primary"> Submit </button>
         </form>
+    )
+
+
+    const showError = () => (
+        <div className="alert alert-danger" style={{display: error ? '' :"none"}}>
+            {error}
+        </div>
+    )
+
+    const showSuccess = () => (
+        <div className="alert alert-info" style={{display: success ? '' :"none"}}>
+            Created Successfuly , please <Link to="/signin"> SignIn</Link>
+        </div>
     )
 
     return (
@@ -69,6 +117,8 @@ const Signup = () => {
             description="SignUp for our App"
             className="container col-md-8 offset-md-2"
             >
+                {showSuccess()}
+                {showError()}
                {signUpForm()}
 
                {/* To view live changes  */}

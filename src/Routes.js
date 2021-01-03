ec2cf29cc95d17de7150727cc39cc37a61d784b1
asmaahassnasr <asmaa14has@gmail.com>
2021-01-03 17:02:43 +0200

    Adding Home Comp and Bootstrap Link

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,13 @@ import React from 'react'
 import {BrowserRouter,Switch,Route} from 'react-router-dom'
 import Signup from './User/Signup'
 import Signin from './User/Signin'
+import Home from './Core/Home'
 
 const Routes = () =>{
     return (
         <BrowserRouter>
             <Switch>
+                <Route exact path="/" component={Home} />
                 <Route path="/signin" component={Signin}/>
                 <Route path="/signup" component={Signup}/>
             </Switch>
